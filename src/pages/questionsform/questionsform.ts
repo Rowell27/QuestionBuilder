e8f63a9c8c95dd2ed9980c74a0893a8c18811fc6
aysonsteven <aysonsteven@gmail.com>
2016-10-17 23:38:44 +0800

    commit

--- src/pages/questionsform/questionsform.ts
+++ src/pages/questionsform/questionsform.ts
@@ -27,23 +27,6 @@ export class Questionsform {
   ionViewDidLoad() {
     console.log("PostListPage::ionViewDidLoad()", this.postListComponent.slug);
   }
-  
 
-  /**
-   * 부모 컴포넌트에서만 ion-infinite-scroll 을 사용 할 수 있으므로
-   * 부모 컴포넌트에서 자식 컴포넌트의 endless loading 로직을 호출 한 후,
-   * complete() 을 호출 한다.
-   * 더 이상 데이터가 없으면, enable(false) 를 호출 한다.
-   */
-  doInfinite( infiniteScroll ) {
-    console.log("PostListPage::doInfinite() begin");
-    this.postListComponent.doInfinite( ( more ) => {
-      console.log("PostListPage::doInfinite() end");
-      infiniteScroll.complete();
-      if ( ! more ) {
-        infiniteScroll.enable( false );
-      }
-    });
-  }
   
 }
